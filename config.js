5240aca1af7d71152e4f75143983fd7fbec3e84b
Bhavya Kohli <kohlibhavya18@gmail.com>
2021-12-19 22:57:35 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC9YDQaBbJ7P7RJKlPs4-CZumMgNnyWe6w",
+    authDomain: "react-native-firebase-da419.firebaseapp.com",
+    projectId: "react-native-firebase-da419",
+    storageBucket: "react-native-firebase-da419.appspot.com",
+    messagingSenderId: "838421802074",
+    appId: "1:838421802074:web:e9707f48bd6cac0eac7c63"
+  };
